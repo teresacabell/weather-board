6e35d937090b62c633e38f0889f55a5b7936e70c
teresacabell <teresacabll@gmail.com>
2021-12-15 15:08:20 -0600

    added some const

---
+++ assets/java.js
@@ -0,0 +1,6 @@
+const searchButtonEl = document.querySelector("#submit-button")
+const searchHistory = document.querySelector("#cities-searched")
+const todayForecast = document.querySelector("#current-weather")
+const futureForecast = document.querySelector("#weather-forecast")
+
+const apiKey = 
