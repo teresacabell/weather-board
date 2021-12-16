0c316d0aebd121db1d8436fb4dee03aadd68a37e
teresacabell <teresacabll@gmail.com>
2021-12-16 11:41:30 -0600

    added guidelines of what i should do

--- assets/java.js
+++ assets/java.js
@@ -25,16 +25,58 @@ var formSubmitHandler = function(event) {
     addPastCity(citySearch);
 }
 
+// save weather to an array
 var saveCitySearch = function(){
     localStorage.setItem("citiesArr", JSON.stringify("citiesArr"));
 };
 
+// retrieve weather
 var getWeather = function(citySearch) {
     var apiKey = "d5d5a1652a4570f6a0810fd859cf2fbd"
     var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
 
     fetch(apiURL)
+    .then(function(response)){
+        response.JSON().then(function(data)){
+        showWeather(data,citySearch);
+        };
+    });
+};
+  
+// get lat and long
+// get UV
+
+
+// display weather
+var showWeather = function(){
+
+
+    // create date
+
+    // create icon
+
+    //make something to hold temp & append
+
+    // make something to hold humidity & append
+
+    // make someting to hold wind & append
+
+    // display uv
+
 }
 
+// get forecast
 
+// display forecast
 
+    // create date & append
+    // create icon & append
+    //create temp & append
+    // create humdity & append
+    
+    // append all to forecast container
+
+// past search
+// past search handler
+
+// call functions
