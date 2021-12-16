3315e134d304cc40bad2720e243f0cd3396fb081
teresacabell <teresacabll@gmail.com>
2021-12-15 19:00:15 -0600

    think I figured out submit handler?

--- assets/java.js
+++ assets/java.js
@@ -1,7 +1,30 @@
 var cities = [];
 
-var citySearchEl = document.querySelector("#city-form");
-var cityInputEl = document.querySelector("#searched-city");
-var currentContainerEl = document.querySelector("")
+var cityFormEl = document.querySelector("#city-form");
+var citySearchEl = document.querySelector("#searched-city");
+var currentContainerEl = document.querySelector("#current-weather");
+var currentCity = document.querySelector("#current-city");
+var forecastEl = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#forecast-cards");
+var pastCitiesEl = document.querySelector("#past-cities");
 
-const apiKey = "d5d5a1652a4570f6a0810fd859cf2fbd"
+var apiKey = "d5d5a1652a4570f6a0810fd859cf2fbd"
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var citySearch = citySearchEl.value.trim();
+
+    if(citySearch) {
+        getWeather(citySearch);
+        getForecast(citySearch);
+    } else {
+        alert("Please search for a city!");
+    }
+    saveCitySearch();
+    addPastCity(citySearch);
+}
+
+
+
+
