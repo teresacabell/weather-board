372ae92996aa721c33817f304c48a5ef29299ab2
teresacabell <teresacabll@gmail.com>
2021-12-17 20:34:50 -0600

    testing 123

--- assets/java.js
+++ assets/java.js
@@ -2,12 +2,11 @@ var searchButtonEl = document.querySelector("#submit-button");
 var searchCityEl = document.querySelector("#searched-city");
 var citySearchFormEl = document.querySelector("#city-form");
 var displayEl = document.querySelector("#display-weather");
-var cityEl = document.querySelector("#cityEl");
 var temperatureEl = document.querySelector("#temperature");
 var windEl = document.querySelector("#wind");
 var humidityEl = document.querySelector("#humidity");
 var uvEl = document.querySelector("#uv");
-
+var todayDate = new Date;
 
 // api key
 var apiKey = "d5d5a1652a4570f6a0810fd859cf2fbd"
@@ -15,21 +14,22 @@ var apiKey = "d5d5a1652a4570f6a0810fd859cf2fbd"
 // search for city 
 var searchCity = function(event) {
     event.preventDefault();
-    var city = searchCityEl.ariaValueMax.trim();
-    searchCity(city)
+    var city = searchCityEl.value.trim();
+    findCity(city)
 }
 searchButtonEl.addEventListener('click', searchCity);
 
+
 // finds city
-var findCity = function(cityName) {
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&sppid=" + apiKey;
-        fetch(apiURL)
+var findCity = function(namedCity) {
+    var apiCityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + namedCity + "&appid=" + apiKey;
+        fetch(apiCityURL)
         .then(response => {
             if (response.ok) {
                 response.json()
                 .then(function(data) {
-                    city.innerHTML = cityName
-                    getWeather(data.coord.lat, data.coord.lon)
+                    city.innerHTML = namedCity
+                    pullWeather(data.coord.lat, data.coord.lon)
                 }) 
             }
         })
@@ -37,15 +37,15 @@ var findCity = function(cityName) {
 
 // gather data
     var pullWeather = function(lat,lon){
-        var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
-        fetch(apiURL)
+        var apiLatLonURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+        fetch(apiLatLonURL)
         .then(response => {
             response.json()
             .then(function(data) {
                 console.log(data)
                 temperatureEl.textContent = "Temperature is " + data.current.temp + "degrees Fahrenheit"
                 windEl.textContent = "Wind is " + data.current.wind_speed + "MPH"
-                humidityEl.textContent = "Humidity is " data.current.uv + "%"
+                humidityEl.textContent = "Humidity is " + data.current.uv + "%"
                 uvEl.textContent = "UV Index is " + data.current.uv
             })
         })
